e34f810181406c1f884811a1bf3321e8d26d24d4
enaho murphy <murphy.enaho@andela.com>
2017-02-28 10:56:07 +0100

    abstracts api response status

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,26 +1,55 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import sourcemaps from 'gulp-sourcemaps';
+import eslint from 'gulp-eslint';
+import mocha from 'gulp-mocha';
 import path from 'path';
+import nodemon from 'gulp-nodemon';
 
 const paths = {
-  es6: './es6/**',
+  server: './server/**',
+  client: './client',
   es5: './es5',
-  sourceRoot: path.join(__dirname, 'es6'),
+  sourceRoot: path.join(__dirname, 'es5'),
+  test: './test/**',
+  eslint: ['server/**/*.js', 'client/**/*.js', 'test/**/**/*.js']
 };
 
 
-gulp.task('babel', () => {
-  return gulp.src(paths.es6)
+gulp.task('lint', () => gulp.src(paths.eslint)
+    .pipe(eslint())
+    .pipe(eslint.format()));
+
+gulp.task('babel:server', () => gulp.src(paths.server)
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(sourcemaps.write('.', { sourceRoot: paths.sourceRoot }))
-    .pipe(gulp.dest(paths.es5));
-});
+    // .pipe(sourcemaps.write('.', { sourceRoot: paths.sourceRoot }))
+    .pipe(gulp.dest(`${paths.es5}`)));
 
-gulp.task('watch', ['babel'], () => {
-  gulp.watch(paths.es6, ['babel']);
-});
+gulp.task('babel:test', () => gulp.src(paths.test)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    // .pipe(sourcemaps.write('.', { sourceRoot: paths.sourceRoot }))
+    .pipe(gulp.dest(`${paths.sourceRoot}/test`)));
 
+gulp.task('start', ['babel'], () => nodemon({
+  script: `${paths.sourceRoot}/server.js`,
+  ext: 'js html',
+  ignore: [`${paths.es5}`],
+  env: { NODE_ENV: 'development' },
+  tasks: ['babel']
+}));
 
-gulp.task('default', ['watch']);
+gulp.task('babel', ['babel:test', 'babel:server']);
+
+gulp.task('watch', () => gulp
+  .watch(paths.es6, ['babel', 'test']));
+
+gulp.task('test:server', ['babel'], () => gulp
+    .src('es5/test/server/**/*.js', { read: false })
+    .pipe(mocha({ reporter: 'nyan' })));
+
+gulp.task('test', ['test:server'], () => gulp
+  .watch('./test/**/**', ['test:server']));
+
+gulp.task('default', ['babel', 'watch']);
---
+++ server/app/controllers/ApiResponse.js
@@ -0,0 +1,146 @@
+/**
+ * @class
+ */
+export default class Response {
+
+  /**
+   * @constructor
+   */
+  constructor() {
+    this.status = 200;
+
+    this.setStatus = this.setStatus.bind(this);
+    this.getStatus = this.setStatus.bind(this);
+  }
+  /**
+   * Sets status code response
+   * @param  {Number} status
+   * @return {Object} object
+   */
+  setStatus(status) {
+    this.status = status;
+    return this;
+  }
+
+  /**
+   * gets status code response
+   * @return {Number} object
+   */
+  getStatus() {
+    return this.status;
+  }
+
+  /**
+   * Sends a response to client
+   * @param  {Object} res - response object
+   * @param  {Object} data - response data
+   * @returns {Object}  response to be sent to client
+   */
+  respond(res, data) {
+    return res.status(this.getStatus())
+      .json(data);
+  }
+
+  /**
+   * Method for 200 response.
+   * @param  {Object} res - response object
+   * @param  {Object} data - data sent back to user
+   * @param {String} message - message about response
+   * @returns {Object}  response to be sent to client
+   */
+  success(res, data = [], message = null) {
+    return this.setStatus(200)
+      .respond(res, {
+        status: true,
+        message,
+        data
+      });
+  }
+
+  /**
+   * Sends a 404 response to client
+   * @param  {Object} res - response object
+   * @param {String} message - message about response
+   * @returns {Object}  response to be sent to client
+   */
+  notFound(res, message) {
+    return this.setStatus(404)
+      .respond(res, {
+        status: false,
+        message
+      });
+  }
+
+  /**
+   * Sends a 500 response to client
+   * @param  {Object} res - response object
+   * @param {String} message - message about response
+   * @returns {Object}  response to be sent to client
+   */
+  serverError(res, message) {
+    return this.setStatus(500)
+      .respond(res, {
+        status: false,
+        message
+      });
+  }
+
+  /**
+   * Sends a 400 response to client
+   * @param  {Object} res - response object
+   * @param {String} message - message about response
+   * @returns {Object}  response to be sent to client
+   */
+  badRequest(res, message) {
+    return this.setStatus(400)
+      .respond(res, {
+        status: false,
+        message
+      });
+  }
+
+  /**
+   * Sends a 401 response to client
+   * @param  {Object} res - response object
+   * @param {String} message - message about response
+   * @returns {Object}  response to be sent to client
+   */
+  unAuthorize(res, message) {
+    return this.setStatus(401)
+      .respond(res, {
+        status: false,
+        message
+      });
+  }
+
+  /**
+   * Sends a 403 response to client
+   * @param  {Object} res - response object
+   * @param {String} message - message about response
+   * @returns {Object}  response to be sent to client
+   */
+  forbidden(res, message) {
+    return this.setStatus(403)
+      .respond(res, {
+        status: false,
+        message
+      });
+  }
+
+
+  /**
+   * Sends a 201 response to client
+   * @param  {Object} res - response object
+   * @param  {Object} data - data sent back to user
+   * @param {String} message - message about response
+   * @returns {Object}  response to be sent to client
+   */
+  created(res, data, message = null) {
+    return this.setStatus(201)
+      .respond(res, {
+        status: true,
+        message: message || 'Resource created',
+        data
+      });
+  }
+}
