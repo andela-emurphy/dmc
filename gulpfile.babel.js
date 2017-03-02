a900d82d3c537c0c31972ce6f1a304c93826d395
andela-emurphy <murphy.enaho@andela.com>
2017-03-02 13:40:05 +0100

    Revert "jwt authentication"

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,50 +1,26 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import eslint from 'gulp-eslint';
-import mocha from 'gulp-mocha';
+import sourcemaps from 'gulp-sourcemaps';
 import path from 'path';
-import nodemon from 'gulp-nodemon';
 
 const paths = {
-  server: './server/**',
-  client: './client',
+  es6: './es6/**',
   es5: './es5',
-  sourceRoot: path.join(__dirname, 'es5'),
-  test: './test/**',
-  eslint: ['server/**/*.js', 'client/**/*.js', 'test/**/**/*.js']
+  sourceRoot: path.join(__dirname, 'es6'),
 };
 
 
-gulp.task('lint', () => gulp.src(paths.eslint)
-    .pipe(eslint())
-    .pipe(eslint.format()));
-
-gulp.task('babel:server', () => gulp.src(paths.server)
+gulp.task('babel', () => {
+  return gulp.src(paths.es6)
+    .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(gulp.dest(`${paths.es5}`)));
+    .pipe(sourcemaps.write('.', { sourceRoot: paths.sourceRoot }))
+    .pipe(gulp.dest(paths.es5));
+});
 
-gulp.task('babel:test', () => gulp.src(paths.test)
-    .pipe(babel())
-    .pipe(gulp.dest(`${paths.sourceRoot}/test`)));
+gulp.task('watch', ['babel'], () => {
+  gulp.watch(paths.es6, ['babel']);
+});
 
-gulp.task('start', ['babel'], () => nodemon({
-  script: `${paths.sourceRoot}/server.js`,
-  ext: 'js html',
-  ignore: [`${paths.es5}`],
-  env: { NODE_ENV: 'development' },
-  tasks: ['babel']
-}));
 
-gulp.task('babel', ['babel:test', 'babel:server']);
-
-gulp.task('watch', () => gulp
-  .watch(paths.es5, ['babel', 'test']));
-
-gulp.task('test:server', ['babel'], () => gulp
-    .src('es5/test/server/**/*.js')
-    .pipe(mocha({ reporter: 'nyan' })));
-
-gulp.task('test', ['test:server'], () => gulp
-  .watch('./test/**/**', ['test:server']));
-
-gulp.task('default', ['babel', 'watch']);
+gulp.task('default', ['watch']);
